53ae734a588baf2add38f954272660ea4b264142
Francesco <francesco.barbato24@gmail.com>
2024-12-11 12:57:56 +0100

    a

--- src/pages/components/TopHeader.jsx
+++ src/pages/components/TopHeader.jsx
@@ -1,11 +1,12 @@
 const TopHeader = () => {
-  return (
-    <div className="bg-blue-700 text-white text-sm py-2">
-      <div className="container mx-auto px-4 lg:px-0">
-        <span className="font-semibold">Ministero della Giustizia</span>
+    return (
+      <div className="bg-blue-700 text-white text-sm py-2 px-4">
+        <div className="container mx-auto">
+          <span className="font-semibold">Ministero della Giustizia</span>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default TopHeader;
+    );
+  };
+  
+  export default TopHeader;
+  
